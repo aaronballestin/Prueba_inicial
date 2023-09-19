0f2a8b340cedda18677baf501a93ea53a9601cc3
aaronballestin <119449828+aaronballestin@users.noreply.github.com>
2023-09-19 20:57:26 +0200

    Hecho todo el javaScript y acabado

--- prueba/js/service_usuario.js
+++ prueba/js/service_usuario.js
@@ -1,3 +1,14 @@
-function myfunction(){
-    
+function desplegarMenu(){
+    var cad = "";
+    var divProductos = document.querySelector(".mostrar");
+
+    cad += "<div class='opciones-movil'>";
+    cad += "<p>Home</p>";
+    cad += "<p>About</p>";
+    cad += "<p>Projects</p>";
+    cad += "<p>Contract</p>";
+
+
+    cad += "</div>";
+    divProductos.innerHTML = cad;
 }
